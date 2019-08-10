c2d7ffe19371171eb0aef9bf8e605c28ca14db2a
astrocat879 <46911128+astrocat879@users.noreply.github.com>
2019-08-09 22:03:03 -0400

    Create cco99p2.py

---
+++ CCO/cco99p2.py
@@ -0,0 +1,36 @@
+import operator
+
+
+def abr(n):
+    if n%10 == 1 and n%100 != 11:
+        return str(n)+"st"
+    if n%10 == 2 and n%100 != 12:
+        return str(n)+"nd"
+    if n%10 == 3 and n%100 != 13:
+        return str(n)+"rd"
+    return str(n)+"th"
+
+
+for _ in range(int(input())):
+    w, k = map(int, input().split())
+    occ = {}
+    for i in range(w):
+        a = input()
+        if a not in occ:
+            occ[a] = 0
+        occ[a] -= 1
+    print(abr(k)+" most common word(s):")
+    occ2 = sorted(occ.items(), key=operator.itemgetter(1))
+    rank = 1
+    smh = 1
+    prev = None
+    for i, x in occ2:
+        if not prev == None and occ[i] != occ[prev]:
+            rank = smh
+        if k == rank:
+            print(i)
+        prev = i
+        smh += 1
+    print()
+
+# smh
